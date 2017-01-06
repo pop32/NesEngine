3c4fcbe609731d1a2a2df0b75beb59b895fa0491
pop32 <higurashi07exp@gmail.com>
2017-01-07 03:10:53 +0900

    CHRエディタ追加

---
+++ gui/CChrEditor.cpp
@@ -0,0 +1,21 @@
+/*
+ * CChrEditor.cpp
+ *
+ *  Created on: 2017/01/07
+ *      Author: kyon
+ */
+
+#include "CChrEditor.h"
+
+namespace NesEngine {
+
+CChrEditor::CChrEditor() {
+	// TODO 自動生成されたコンストラクター・スタブ
+
+}
+
+CChrEditor::~CChrEditor() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace NesEngine */
